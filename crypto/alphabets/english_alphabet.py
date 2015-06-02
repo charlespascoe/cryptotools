5e4c2551e887293a9a67b0df99dffb6b35826312
Charles Pascoe <cpascoe95@gmail.com>
2015-06-02 14:10:55 +0100

    Changed EnglishAlphabet to inherit from AlphabetWithProbabilities

--- crypto/alphabets/english_alphabet.py
+++ crypto/alphabets/english_alphabet.py
@@ -1,9 +1,12 @@
-from crypto.alphabets import Alphabet
+from crypto.alphabets import AlphabetWithProbabilities
 import string
 
-class EnglishAlphabet(Alphabet):
+class EnglishAlphabet(AlphabetWithProbabilities):
     def __init__(self):
         self._alph = string.ascii_uppercase
 
     def strip(self, text):
         return super().strip(text.upper())
+
+    def init_probs(self):
+        raise NotImplemented()
