574a6071f49febe0b64371c06a118a06e18d0e80
Charles Pascoe <cpascoe95@gmail.com>
2015-06-09 17:45:00 +0100

    Added Strip, Iter, Str, and Contains tests

--- tests/alphabet_tests.py
+++ tests/alphabet_tests.py
@@ -5,7 +5,7 @@ import string
 class AlphabetTests(unittest.TestCase):
     def setUp(self):
         self.alph = crypto.alphabets.Alphabet()
-        self.alph = string.ascii_uppercase
+        self.alph._alph = string.ascii_uppercase
 
     def test_indexing(self):
         self.assertEqual(self.alph[0], 'A')
@@ -17,3 +17,19 @@ class AlphabetTests(unittest.TestCase):
 
     def test_len(self):
         self.assertEqual(len(self.alph), 26)
+
+    def test_strip(self):
+        self.assertEqual(self.alph.strip('AbCdEf123'), 'ACE')
+
+    def test_iter(self):
+        s = ''.join([letter for letter in self.alph])
+
+        self.assertEqual(s, string.ascii_uppercase)
+
+    def test_str(self):
+        self.assertEqual(str(self.alph), string.ascii_uppercase)
+
+    def test_contains(self):
+        self.assertTrue('A' in self.alph)
+        self.assertFalse('a' in self.alph)
+        self.assertFalse('1' in self.alph)
