02404c5900eb0676fbcb53e80662ab3b4988e0d9
Charles Pascoe <cpascoe95@gmail.com>
2015-06-10 11:28:09 +0100

    EnglishAlphabet now makes call to super constructor

--- crypto/alphabets/english_alphabet.py
+++ crypto/alphabets/english_alphabet.py
@@ -3,6 +3,7 @@ import string
 
 class EnglishAlphabet(AlphabetWithProbabilities):
     def __init__(self):
+        super().__init__()
         self._alph = string.ascii_uppercase
 
     def strip(self, text):
