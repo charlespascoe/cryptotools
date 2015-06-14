9a5d1795b473618c13eb1018a3540bab4d5c5c60
Charles Pascoe <cpascoe95@gmail.com>
2015-06-14 08:11:39 +0100

    Added basic bash completion

---
+++ ct_bash_completion.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+_cryptotools_opts () {
+    local cur prev opts
+    COMPREPLY=()
+
+    cur=${COMP_WORDS[COMP_CWORD]}
+
+    if (( $COMP_CWORD == 1 )) ; then
+        COMPREPLY=( $(compgen -W 'ciphers analysis' -- $cur))
+    fi;
+
+    if (( $COMP_CWORD >= 2)) ; then
+        action=${COMP_WORDS[1]}
+        case "$action" in
+            'ciphers')
+                COMPREPLY=($(compgen -W 'caesar-shift affine-shift' -- $cur)) ;;
+            'analysis')
+                COMPREPLY=($(compgen -W 'caesar-shift-cracker' -- $cur)) ;;
+        esac;
+    fi;
+
+    return 0;
+}
